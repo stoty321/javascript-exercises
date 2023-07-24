afb0d8e7c27a38996fb0e50fdf8f9b5647529305
stoty321 <stoty321@gmail.com>
2023-07-24 16:20:21 +0100

    completed fibonacci exercise
    created an if statement for the number to be greater than 0
    created a for loop to count through the fibonacci sequence and return
    the value at that possition.
    returned OOPS if number isnt possitive.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+if(number > 0){
+
+    let n1 = 0, n2 = 1, nextTerm = 1;
+
+    for (let i = 1; i < number; i++){
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return nextTerm;
+}
+return "OOPS";
+
 
 };
 
