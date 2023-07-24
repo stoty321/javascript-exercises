a98edff2df6da50d09513f3f519f99e52f4ef5af
stoty321 <stoty321@gmail.com>
2023-07-24 15:51:19 +0100

    completed palindromes exercise used str.replace to removed non alphanumerical values in the string and remove spaces.
    used .toLowerCase to make the string lower case.
    created a function to reverse the string
    used a for loop to check each value in the string was the same.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newString = "";
+    newString = str.replace(/[^0-9a-zA-Z]/gi,"").replace(/ /g,"");
+    newString = newString.toLowerCase();
+    
+    let revString = reverseString(newString);
+
+    for(let i = 0; i < newString.length; i++){
+        if(newString[i] !== revString[i]){
+            return false
+        }
+    }
+    return true;
 
 };
+
+function reverseString(str){
+   let newString = "";
+
+   for(let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+   }
+
+   return newString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
