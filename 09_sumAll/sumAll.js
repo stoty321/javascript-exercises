e29634ed6a433d2b6c9f3c8f3ecaeb4f383ecf67
Stoty <104146125+stoty321@users.noreply.github.com>
2025-09-29 21:34:07 +0100

    add - completed sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b)
+        || a < 0 || b < 0
+    )   
+    return 'ERROR';
 
+
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for(; a <= b; a++){
+        sum += a;
+    }
+    return sum;
+    //return (b - a + 1) * (a + b) / 2;
 };
 
 // Do not edit below this line
