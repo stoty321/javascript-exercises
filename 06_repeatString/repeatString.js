6bc50f4228811d9e4f44c9e3431c8fa40176b259
Stoty <104146125+stoty321@users.noreply.github.com>
2025-09-29 17:56:52 +0100

    add - completed repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
