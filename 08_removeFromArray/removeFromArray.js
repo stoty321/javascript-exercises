2d398af7ece7799de973d14b6495088cf5f81ede
Stoty <104146125+stoty321@users.noreply.github.com>
2025-09-29 20:53:20 +0100

    add - completed removeFromArray in a weird way.
    but it worked

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let removeIndex;
+    for (item of remove){
+        do{
+            removeIndex = (array.findIndex((index) => index === item));
+            if(removeIndex !== -1){
+            array.splice(removeIndex, 1);
+            }
+        }while(removeIndex !== -1)
+    }
+
+    return array;
 };
 
 // Do not edit below this line
