0ad1c1dd0d559cec95c9db65ff8d5caea1eae8a7
Stoty <104146125+stoty321@users.noreply.github.com>
2025-10-09 13:56:53 +0100

    add - completed calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for(const value of arr){
+    total += value;
+  }
+  return total;
+
+  //could have used reduce instead
+  //return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+    let total = 1;
+	for(const value of arr){
+    total *= value;
+  }
+  return total;
 
+  //could have used reduce instead
+  // return arr.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let total = 1;
+  for (let i = 0; i < y; i++){
+    total *= x;
+  }
+  return total;
+
+  //could have used the power operator instead
+  // return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  for (let i = x; i > 0; i--){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
