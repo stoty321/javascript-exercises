c614077ace7f297e3351116c0b32a5b8d5b1cef8
Stoty <104146125+stoty321@users.noreply.github.com>
2025-10-10 01:23:26 +0100

    add - completed palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // str = str.toLowerCase();
+    // str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    // str = str.replaceAll(" ","");
+    // const reverseStr = str.split("").reverse().join("");
+    // return reverseStr == str ? true: false;
 
+    //a better way to do it would be:
+    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversed = cleaned.split("").reverse().join("");
+    return cleaned === reversed;
 };
 
 // Do not edit below this line
