7548b556e4dd8d5ac472e7097396d6bae0876686
Stoty <104146125+stoty321@users.noreply.github.com>
2025-10-10 12:10:45 +0100

    add - completed fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = +x;
+    
+    let first = 0; 
+    let second = 1;
+    let sum = 0;
 
+    if(x < 0) return "OOPS";
+    if(x === 1) return 1;
+
+    for(let i = 1; i < x; i++){
+        sum = (first + second);
+        first = second;
+        second = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
