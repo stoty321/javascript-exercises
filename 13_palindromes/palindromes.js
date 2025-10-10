156ae7fe6d9300adb873bf838aefbf636cb6c33e
Stoty <104146125+stoty321@users.noreply.github.com>
2025-10-10 12:15:26 +0100

    add - completed getTheTitles exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,14 +1,14 @@
 const palindromes = function (str) {
-    // str = str.toLowerCase();
-    // str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-    // str = str.replaceAll(" ","");
-    // const reverseStr = str.split("").reverse().join("");
-    // return reverseStr == str ? true: false;
+    str = str.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    str = str.replaceAll(" ","");
+    const reverseStr = str.split("").reverse().join("");
+    return reverseStr == str ? true: false;
 
     //a better way to do it would be:
-    const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
-    const reversed = cleaned.split("").reverse().join("");
-    return cleaned === reversed;
+    // const cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // const reversed = cleaned.split("").reverse().join("");
+    // return cleaned === reversed;
 };
 
 // Do not edit below this line
